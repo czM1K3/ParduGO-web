6f3f8c4e529f249afac85acdb4a6c86330c727ba
czM1K3 <misamadera1@gmail.com>
2021-11-11 22:02:36 +0100

    Added backend functions

--- lib/type-defs.ts
+++ lib/type-defs.ts
@@ -3,5 +3,42 @@ import { gql } from "@apollo/client";
 export default gql`
 	type Query {
 		hello: String!
+		getAllEvents: [Event!]!
+		getUserEvents: [Event!]!
+	}
+
+	type Mutation {
+		login(email: String!, password: String!): String!
+		register(email: String!, password: String!): String!
+		createEvent(input: CreateEventInput!): Boolean!
+		approveEvent(id: ID!): Boolean!
+	}
+
+	input CreateEventInput {
+		name: String!
+		latitude: Float!
+		longitude: Float!
+		startTime: String!
+		endTime: String!
+		category: String!
+		web: String!
+		contactPhone: String!
+		contactEmail: String!
+		description: String!
+	}
+
+	type Event {
+		id: ID!
+		name: String!
+		latitude: Float!
+		longitude: Float!
+		start: String!
+		end: String!
+		category: String!
+		web: String!
+		contact_phone: String!
+		contact_email: String!
+		description: String!
+		approved: Boolean!
 	}
 `;
