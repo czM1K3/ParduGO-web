697ebdc6854e1541c271135cf74aef4429f72bd6
czM1K3 <misamadera1@gmail.com>
2021-11-11 17:42:03 +0100

    Basic graphql

---
+++ lib/types/token.ts
@@ -0,0 +1,3 @@
+export type MyToken = {
+	userId: number;
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,16 @@ import '../styles/globals.css';
 import 'tailwindcss/tailwind.css';
 
 import type { AppProps } from 'next/app';
+import { ApolloProvider } from '@apollo/client';
+import { useApollo } from 'lib/apollo';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const apolloClient = useApollo(pageProps.initialApolloState);
+  return (
+    <ApolloProvider client={apolloClient}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
 
 export default MyApp;
