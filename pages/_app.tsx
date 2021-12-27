6dbfb9142b18893cdb9474ef8d3b9dc830001277
czM1K3 <misamadera1@gmail.com>
2021-12-27 22:41:44 +0100

    Added loading bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,11 +5,15 @@ import type { AppProps } from 'next/app';
 import { ApolloProvider } from '@apollo/client';
 import { useApollo } from 'lib/apollo';
 import { FavoriteProvider } from 'utils/favoriteContext';
+import ProgressBar from "nextjs-progressbar";
 
 function MyApp({ Component, pageProps }: AppProps) {
 	const apolloClient = useApollo(pageProps.initialApolloState);
 	return (
 		<ApolloProvider client={apolloClient}>
+			<ProgressBar color='#B70106' options={{
+				showSpinner: false,
+			}} />
 			<FavoriteProvider>
 				<Component {...pageProps} />
 			</FavoriteProvider>
