4833bf1fae671944c75852b381d7daa2cc86a3f0
czM1K3 <misamadera1@gmail.com>
2022-02-20 13:32:48 +0100

    Added preact

--- next.config.js
+++ next.config.js
@@ -7,6 +7,15 @@ module.exports = {
 			exclude: /node_modules/,
 			use: [options.defaultLoaders.babel, { loader: 'graphql-let/loader' }],
 		});
+
+		if (!options.dev && !options.isServer) {
+			Object.assign(config.resolve.alias, {
+				react: 'preact/compat',
+				'react-dom/test-utils': 'preact/test-utils',
+				'react-dom': 'preact/compat',
+			});
+		}
+
 		return config;
 	},
 	productionBrowserSourceMaps: true,
